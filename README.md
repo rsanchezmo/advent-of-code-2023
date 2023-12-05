f341ecb6b7aa853415bfdb87bbcbdd67d591230a
Sánchez Molina, Rodrigo <rsanchezmo@indra.es>
2023-12-05 20:36:20 +0100

    solve 2 day

--- README.md
+++ README.md
@@ -2,4 +2,6 @@
 Advent of Code 2023 in Python https://adventofcode.com/
 
 ## Days completed
-- Day 1: ```december_1.py ```
+- Day 1: ```december_1.py ``` ✔️
+- Day 2: ```december_2.py ``` ✔️
+- Day 3: ```december_3.py ``` 
--- december_1/december_1.py
+++ december_1/december_1.py
@@ -42,6 +42,12 @@ What is the sum of all of the calibration values?
 
 import re
 
+def get_number(line: str) -> int:
+    digits = re.findall(r'\d', line)
+    digits = [digits[0], digits[-1]] if len(digits) > 1 else digits[0] * 2
+    number = int(''.join(digits))
+    return number
+
 
 def main():
     with open('./december_1/calibration.txt') as f:
@@ -51,16 +57,27 @@ def main():
     print("Part 1")
     sum_num = 0
     for line in example_input:
-        digits = re.findall(r'\d', line)
-        digits = [digits[0], digits[-1]] if len(digits) > 1 else digits[0] * 2
-        number = int(''.join(digits))
-        print(f"Number: {number}")
+        number = get_number(line)
         sum_num += number
 
     print(f"The sum of all calibration numbers is: {sum_num}")
 
     print("Part 2")
+    sum_num = 0
+    word_to_num = {
+        'one': 'o1e', 'two': 't2o', 'three': 'th3e', 'four': '4', 'five': '5e',
+        'six': '6', 'seven': 's7n', 'eight': 'e8t', 'nine': '9e'
+    }
+
+    for line in example_input:
+        for word, num in word_to_num.items():
+            line = line.replace(word, num)
+        number = get_number(line)
+        sum_num += number
+    print(f"The sum of all calibration numbers is: {sum_num}")
     
+
+
 
 
 if __name__ == '__main__':
