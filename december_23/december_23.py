1d80566c70e8bb8255333209d23bef52f26a723a
Rodrigo Sánchez Molina <rsanchezm98@gmail.com>
2023-12-29 10:07:17 +0100

    ckpt day 23

--- december_23/december_23.py
+++ december_23/december_23.py
@@ -1,12 +1,77 @@
+from dataclasses import dataclass
+from typing import Tuple
+from collections import deque
 
+@dataclass
+class Node:
+    pose: Tuple[int, int]
+    parent_pose: Tuple[int, int]
+    cost: int
+
+    def __hash__(self):
+        return hash((*self.pose, self.cost))
 
 def main():
     with open('./december_23/test.txt') as f:
         lines = f.readlines()
 
-    for line in lines:
-        ...
+    map_ = []
+    start, goal = None, None
+    for i, line in enumerate(lines):
+        line = list(line.strip())
+        map_.append(line)
 
+        if i == 0:
+            start = (line.index('.'), 1) # can ommit first line
+        elif i == len(lines) - 1:
+            goal = (line.index('.'), i)
+
+    
+    open_nodes = deque()
+    closed_nodes = set()
+
+    open_nodes.append(Node(pose=start, parent_pose=(start[0], 0), cost=1))
+
+    MOVES = [(0, 1), (0, -1), (1, 0), (-1, 0)]
+
+    def get_next_nodes(current_node, map_, closed_nodes, open_nodes):
+        next_poses = []
+
+        for move in MOVES:
+            next_pose = (current_node.pose[0] + move[0], current_node.pose[1] + move[1])
+
+            if 0 < next_pose[0] < len(map_[0]) and 0 < next_pose[1] < len(map_):
+                continue
+
+            if next_pose != current_node.parent_pose and map_[next_pose[1]][next_pose[0]] != '#':
+                if map_[next_pose[1]][next_pose[0]] == '>' and move[0] == -1:
+                    continue
+                if map_[next_pose[1]][next_pose[0]] == '<' and move[0] == 1:
+                    continue
+                if map_[next_pose[1]][next_pose[0]] == '^' and move[1] == 1:
+                    continue
+                if map_[next_pose[1]][next_pose[0]] == 'v' and move[1] == -1:
+                    continue
+                
+                next_poses.append(next_pose)
+            else:
+                continue
+
+            node = Node(pose=next_pose, parent_pose=current_node.pose, cost=current_node.cost + 1)
+            if node in closed_nodes:
+                continue
+            open_nodes.append(node)
+
+        return next_poses
+
+    while open_nodes:
+        current = open_nodes.popleft()
+        closed_nodes.add(current)
+        get_next_nodes(current, map_, closed_nodes, open_nodes)
+
+
+    max_cost = max([node.cost for node in closed_nodes])
+    print(f'Part 1: {max_cost}')
 
 if __name__ == '__main__':
     main()
